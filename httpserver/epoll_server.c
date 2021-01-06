7ccf0c3395ebd5fb2644470087b542879df37d5c
GreigHuth <greighuth@hotmail.co.uk>
2021-01-06 21:30:31 +0000

    it works !!!

--- httpserver/epoll_server.c
+++ httpserver/epoll_server.c
@@ -9,10 +9,14 @@
 #include <sys/epoll.h>
 #include <errno.h>
 #include <netinet/in.h> 
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 #define Q_LEN 16
 #define MAX_EVENTS 32
+#define BUFFER_SIZE 1024
+
 
 //set address and port for socket
 static void set_sockaddr(struct sockaddr_in * addr){
@@ -38,15 +42,18 @@ int main(){
     
     struct epoll_event ev, events[MAX_EVENTS];
     int listen_sock, conn_sock, nfds, epollfd;
+    char buf[BUFFER_SIZE];
 
 
     //first we need to set up the address and port struct
 
-    struct sockaddr_in addr;//addr we want to bind the socket to
+    struct sockaddr_in s_addr;//addr we want to bind the socket to
+    struct sockaddr_in c_addr;//address of the client 
 
-    set_sockaddr(&addr);
+    set_sockaddr(&s_addr);
 
-    int addr_len = sizeof(addr);
+    int s_addr_len = sizeof(s_addr);
+    int c_addr_len = sizeof(c_addr);
 
     listen_sock = socket(AF_INET, SOCK_STREAM, 0); //creating fd for socket
 
@@ -58,7 +65,7 @@ int main(){
     printf("Socket created, binding socket...\n");
 
     //bind socket to addr and port given by addr
-    if ( bind(listen_sock, (struct sockaddr *) &addr, addr_len) < 0){
+    if ( bind(listen_sock, (struct sockaddr *) &s_addr, s_addr_len) < 0){
 
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -111,16 +118,18 @@ int main(){
             if (events[n].data.fd == listen_sock){
                 
                 //accept connection to the given socket and set it to non blocking 
-                conn_sock = accept(listen_sock, (struct sockaddr*)&addr, &addr_len);
-
+                conn_sock = accept(listen_sock, (struct sockaddr*)&c_addr, &c_addr_len);
                 if (conn_sock == -1){
                     perror("cannot accept new connection");
                     exit(EXIT_FAILURE);
                 }
 
+                //get info about client
+                printf("new connection from client:%s\n",inet_ntoa(c_addr.sin_addr));
+
                 //add connection to the list of polling 
                 setnonblocking(conn_sock); //make sure new socket is non blocking
-                ev.events = EPOLLIN | EPOLLET;
+                ev.events = EPOLLIN;
                 ev.data.fd = conn_sock;
                 if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1){
                     perror("adding new connection to epoll_ctl failed");
@@ -128,11 +137,20 @@ int main(){
                 }
 
             //if any other fd is available for IO then we do stuff
-            }else{
-
-                //hand IO
-                //TODO
-
+            }else {
+                
+                //read from socket until its empty
+                for (;;){
+                    bzero(buf, sizeof(buf));
+                    if (read(events[n].data.fd, buf, sizeof(buf)) <=0){
+                        //EOF
+                        break;
+                    } else{
+                        //THIS IS WHERE WE DO STUFF WITH THE DATA
+                        printf("[+] data: %s\n", buf);
+                        sleep(1);
+                    }
+                }
             }
         }
     }
