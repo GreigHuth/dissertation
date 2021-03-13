b944fdbbb9208e690337165a4a1cb0403738414c
greighuth <greighuth@hotmail.co.uk>
2021-03-13 20:04:19 +0000

    ran freeBSD experiments

---
+++ freeBSD_bm/single/wrk2csv.lua
@@ -0,0 +1,12 @@
+--NOT MY CODE https://gist.github.com/YutaroHayakawa/7f4a1447bc7d66bb42cd529dfe93a329
+
+done = function(summary, latency, requests)
+  -- open output file
+  f = io.open("ring4_60B_T10.csv", "a+")
+  
+  
+  f:write(string.format("%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d\n",
+  latency.min, latency.max, latency.mean, latency.stdev, latency:percentile(50), latency:percentile(75), latency:percentile(90), latency:percentile(99), summary["duration"], summary["requests"], summary["bytes"]))
+  
+  f:close()
+end
