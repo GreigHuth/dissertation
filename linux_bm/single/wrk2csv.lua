05a52301cd73a25f307954e5789bb34cad553cf2
greighuth <greighuth@hotmail.co.uk>
2021-03-15 23:02:44 +0000

    more benchmarking funfunfunfunfunfun aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- linux_bm/single/wrk2csv.lua
+++ linux_bm/single/wrk2csv.lua
@@ -2,8 +2,7 @@
 
 done = function(summary, latency, requests)
   -- open output file
-  f = io.open("ring4096_1MB_T10.csv", "a+")
-  
+  f = io.open("hwoff_1MB_T10.csv", "a+")  
   
   f:write(string.format("%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d\n",
   latency.min, latency.max, latency.mean, latency.stdev, latency:percentile(50), latency:percentile(75), latency:percentile(90), latency:percentile(99), summary["duration"], summary["requests"], summary["bytes"]))
