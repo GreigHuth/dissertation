58c9ce58f5e2ffc6353f67841dcc6b53044b32a6
greighuth <greighuth@hotmail.co.uk>
2021-03-18 16:44:20 +0000

    moar benchmarks

--- linux_bm/single_better/wrk2csv.lua
+++ linux_bm/single_better/wrk2csv.lua
@@ -2,7 +2,7 @@
 
 done = function(summary, latency, requests)
   -- open output file
-  f = io.open("INM0_ring1024_50KB_T0.csv", "a+")  
+  f = io.open("INM0_ring512_50KB_T0.csv", "a+")  
   
   f:write(string.format("%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d\n",
   latency.min, latency.max, latency.mean, latency.stdev, latency:percentile(50), latency:percentile(75), latency:percentile(90), latency:percentile(99), summary["duration"], summary["requests"], summary["bytes"]))
